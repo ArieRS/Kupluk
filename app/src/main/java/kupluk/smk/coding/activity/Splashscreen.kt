53b7b9ea4777bffd11f8cd1ed4330ac49c48eb3a
Ardi B <ardy1144malang@gmail.com>
2020-07-08 21:33:28 +0700

    splashscreen

---
+++ app/src/main/java/kupluk/smk/coding/activity/Splashscreen.kt
@@ -0,0 +1,22 @@
+package kupluk.smk.coding.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kupluk.smk.coding.R
+
+class Splashscreen : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
