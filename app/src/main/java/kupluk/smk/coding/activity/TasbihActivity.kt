b66888580662fc801760137af876c6bdffe526a4
ariers <arie.rachmad.s@gmail.com>
2020-07-11 08:51:07 +0700

    move tasbih to activity

--- app/src/main/java/kupluk/smk/coding/activity/TasbihActivity.kt
+++ app/src/main/java/kupluk/smk/coding/activity/TasbihActivity.kt
@@ -3,10 +3,29 @@ package kupluk.smk.coding.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kupluk.smk.coding.R
+import kotlinx.android.synthetic.main.fragment_tasbih.*
 
 class TasbihActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tasbih)
+        setContentView(R.layout.fragment_tasbih)
+
+        var awal = 0
+
+        btnplus.setOnClickListener {
+
+            var awal = awal++
+            textView.setText(awal.toString())
+        }
+        btnReset.setOnClickListener {
+
+            if ( awal == 0 ) {
+                textView.setText(awal.toString())
+            } else {
+                awal = 0
+
+            }
+            textView.setText(awal.toString())
+        }
     }
 }
